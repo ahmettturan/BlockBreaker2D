4995ddb66960ec23372342e9b79de98212919021
Ahmet Turan Keçe <ahmet.turankece@hotmail.com>
2022-07-25 15:32:01 +0300

    part3

---
+++ BlockBreaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //config params
+   [SerializeField] PaddleController paddle1;
+   [SerializeField] float ballLaunchX = 2f;
+   [SerializeField] float ballLaunchY = 13f;
+   bool hasStarted = false;
+   
+
+   //state
+   Vector2 paddleToBallVector; 
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+          LockBallToPaddle();
+          LaunchOnMouseClick();  
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+           hasStarted = true;
+           GetComponent<Rigidbody2D>().velocity = new Vector2(ballLaunchX, ballLaunchY); 
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
--- BlockBreaker/Assets/Scripts/PaddleController.cs
+++ BlockBreaker/Assets/Scripts/PaddleController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PaddleController : MonoBehaviour
 {
+    // configuration paramaters
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float screenWidthInUnits = 16f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,9 @@ public class PaddleController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float mousePosInUnits = (Input.mousePosition.x / Screen.width * screenWidthInUnits);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        transform.position = paddlePos;
     }
 }
